4c05423c1b02ce163a5c45e9a16aa6dedaf4a856
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-17 20:21:51 -0300

    Update sysboot.asm

--- núcleo/sysboot.asm
+++ núcleo/sysboot.asm
@@ -2,17 +2,17 @@ CALL "ignition switch signal"
 TEST "Power-Up Display Successfully"
 RET "Standby Display"
 CPU "is in protected mode"
-LODS [EAX], LODSW "space access";
-NOP [AH], LODSB [0000 0000]; Null
-LOD [AH], LODSB [0000 0001]; Start of Header
-LOD [AH], LODSB [0000 0010]; Start of Text
-LOD [AH], LODSB [0000 0011]; End of Text
-LOD [AH], LODSB [0000 0100]; End of Transmission
-LODS [EBX], LODSW "base space";
-LOD [BH], LODSB [0000 0101]; Enquiry
-LOD [BH], LODSB [0000 0110]; Acknowledge
-LOD [BH], LODSB [0000 0111]; Bell
-LOD [BH], LODSB [0000 1000]; BackSpace
+MOVS [EAX], MOVSW "space access";
+NOP [AH], MOVSB [0000 0000]; Null
+MOV [AH], MOVSB [0000 0001]; Start of Header
+MOV [AH], MOVSB [0000 0010]; Start of Text
+MOV [AH], MOVSB [0000 0011]; End of Text
+MOV [AH], MOVSB [0000 0100]; End of Transmission
+MOVS [EBX], MOVSW "base space";
+MOV [BH], MOVSB [0000 0101]; Enquiry
+MOV [BH], MOVSB [0000 0110]; Acknowledge
+MOV [BH], MOVSB [0000 0111]; Bell
+MOV [BH], MOVSB [0000 1000]; BackSpace
 MOVS [EDX], MOVSW "data space";
 MOV [DS], MOVSB [0000 1001]; Horizontal Tabulation
 MOV [DS], MOVSB [0000 1010]; Line Feed
@@ -24,12 +24,12 @@ MOV [CS], MOVSB [0000 1110]; Shift Out
 MOV [CS], MOVSB [0000 1111]; Shift In
 MOV [CS], MOVSB [0001 0000]; Data Link Escape
 SOTS [EBP], STOSW "space stack base";
-MOV [IP], LODSB [0001 0000]; Device Control 1
-MOV [IP], LODSB [0001 0001]; Device Control 2
-MOV [IP], LODSB [0001 0010]; Device Control 3
-MOV [IP], LODSB [0001 0011]; Device Control 4
+MOV [IP], MOVSB [0001 0000]; Device Control 1
+MOV [IP], MOVSB [0001 0001]; Device Control 2
+MOV [IP], MOVSB [0001 0010]; Device Control 3
+MOV [IP], MOVSB [0001 0011]; Device Control 4
 SCAS [ESP], SCASW "stack source space";
-MOV [BL], LODSB [0001 0101]; Negative Acknowledge
-MOV [BL], LODSB [0001 0110]; Synchronous
-MOV [BL], LODSB [0001 0111]; End of Transmission Block
-MOV [BL], LODSB [0001 1000]; Cancel
+MOV [BL], MOVSB [0001 0101]; Negative Acknowledge
+MOV [BL], MOVSB [0001 0110]; Synchronous
+MOV [BL], MOVSB [0001 0111]; End of Transmission Block
+MOV [BL], MOVSB [0001 1000]; Cancel
