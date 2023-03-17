52a455c0cf82f7758ebe72db458f7f65c156fde4
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-17 20:23:18 -0300

    Update sysboot.asm

--- núcleo/sysboot.asm
+++ núcleo/sysboot.asm
@@ -23,12 +23,12 @@ MOV [CS], MOVSB [0000 1101]; Carriage Return
 MOV [CS], MOVSB [0000 1110]; Shift Out
 MOV [CS], MOVSB [0000 1111]; Shift In
 MOV [CS], MOVSB [0001 0000]; Data Link Escape
-SOTS [EBP], STOSW "space stack base";
+MOVS [EBP], MOVSW "space stack base";
 MOV [IP], MOVSB [0001 0000]; Device Control 1
 MOV [IP], MOVSB [0001 0001]; Device Control 2
 MOV [IP], MOVSB [0001 0010]; Device Control 3
 MOV [IP], MOVSB [0001 0011]; Device Control 4
-SCAS [ESP], SCASW "stack source space";
+MOVS [ESP], MOVSW "stack source space";
 MOV [BL], MOVSB [0001 0101]; Negative Acknowledge
 MOV [BL], MOVSB [0001 0110]; Synchronous
 MOV [BL], MOVSB [0001 0111]; End of Transmission Block
