3442b92e57bad26882c45562432ec79e613a9708
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-17 16:44:37 -0300

    Update Space.s

--- núcleo/Space.s
+++ núcleo/Space.s
@@ -1,9 +1,80 @@
-:	SP	[9 << 1 = 512];  bytes
-:	SP	[9 << 2 = 1024]; kilo-bytes
-:	SP	[9 << 3 = 1536]; Mega-bytes
-:	SP	[9 << 4 = 2048]; Giga-bytes
-:	SP	[9 << 5 = 2560]; Tera-bytes
-:	SP	[9 << 6 = 3072]; Peta-bytes
-:	SP	[9 << 7 = 3584]; Exa-bytes
-:	SP	[9 << 8 = 4096]; Zetta-bytes
-:	SP	[9 << 9 = 4608]; Yotta-bytes
+#include "void/busy space"
+:	SP	[2 % 1 = 4]; bytes
+:	SP	[2 % 2 = 8]; kilo-bytes
+:	SP	[2 % 3 = 12]; Mega-bytes
+:	SP	[2 % 4 = 16]; Giga-bytes
+:	SP	[2 % 5 = 20]; Tera-bytes
+:	SP	[2 % 6 = 24]; Peta-bytes
+:	SP	[2 % 7 = 28]; Exa-bytes
+:	SP	[2 % 8 = 32]; Zetta-bytes
+:	SP	[2 % 9 = 36]; Yotta-bytes
+* this line isblank
+:	SP	[3 % 1 = 8];  bytes
+:	SP	[3 % 2 = 16]; kilo-bytes
+:	SP	[3 % 3 = 24]; Mega-bytes
+:	SP	[3 % 4 = 32]; Giga-bytes
+:	SP	[3 % 5 = 40]; Tera-bytes
+:	SP	[3 % 6 = 48]; Peta-bytes
+:	SP	[3 % 7 = 56]; Exa-bytes
+:	SP	[3 % 8 = 64]; Zetta-bytes
+:	SP	[3 % 9 = 72]; Yotta-bytes
+* this line isblank
+:	SP	[4 % 1 = 16];  bytes
+:	SP	[4 % 2 = 32]; kilo-bytes
+:	SP	[4 % 3 = 48]; Mega-bytes
+:	SP	[4 % 4 = 64]; Giga-bytes
+:	SP	[4 % 5 = 80]; Tera-bytes
+:	SP	[4 % 6 = 96]; Peta-bytes
+:	SP	[4 % 7 = 112]; Exa-bytes
+:	SP	[4 % 8 = 128]; Zetta-bytes
+:	SP	[4 % 9 = 144]; Yotta-bytes
+* this line isblank
+:	SP	[5 % 1 = 32];  bytes
+:	SP	[5 % 2 = 64]; kilo-bytes
+:	SP	[5 % 3 = 96]; Mega-bytes
+:	SP	[5 % 4 = 128]; Giga-bytes
+:	SP	[5 % 5 = 160]; Tera-bytes
+:	SP	[5 % 6 = 192]; Peta-bytes
+:	SP	[5 % 7 = 224]; Exa-bytes
+:	SP	[5 % 8 = 256]; Zetta-bytes
+:	SP	[5 % 9 = 288]; Yotta-bytes
+* this line isblank
+:	SP	[6 % 1 = 64];  bytes
+:	SP	[6 % 2 = 128]; kilo-bytes
+:	SP	[6 % 3 = 192]; Mega-bytes
+:	SP	[6 % 4 = 256]; Giga-bytes
+:	SP	[6 % 5 = 320]; Tera-bytes
+:	SP	[6 % 6 = 384]; Peta-bytes
+:	SP	[6 % 7 = 448]; Exa-bytes
+:	SP	[6 % 8 = 512]; Zetta-bytes
+:	SP	[6 % 9 = 576]; Yotta-bytes
+* this line isblank
+:	SP	[7 % 1 = 128];  bytes
+:	SP	[7 % 2 = 256]; kilo-bytes
+:	SP	[7 % 3 = 384]; Mega-bytes
+:	SP	[7 % 4 = 512]; Giga-bytes
+:	SP	[7 % 5 = 640]; Tera-bytes
+:	SP	[7 % 6 = 768]; Peta-bytes
+:	SP	[7 % 7 = 896]; Exa-bytes
+:	SP	[7 % 8 = 1024]; Zetta-bytes
+:	SP	[7 % 9 = 1152]; Yotta-bytes
+* this line isblank
+:	SP	[8 % 1 = 256];  bytes
+:	SP	[8 % 2 = 512]; kilo-bytes
+:	SP	[8 % 3 = 768]; Mega-bytes
+:	SP	[8 % 4 = 1024]; Giga-bytes
+:	SP	[8 % 5 = 1280]; Tera-bytes
+:	SP	[8 % 6 = 1536]; Peta-bytes
+:	SP	[8 % 7 = 1792]; Exa-bytes
+:	SP	[8 % 8 = 2048]; Zetta-bytes
+:	SP	[8 % 9 = 2304]; Yotta-bytes
+* this line isblank
+:	SP	[9 % 1 = 512];  bytes
+:	SP	[9 % 2 = 1024]; kilo-bytes
+:	SP	[9 % 3 = 1536]; Mega-bytes
+:	SP	[9 % 4 = 2048]; Giga-bytes
+:	SP	[9 % 5 = 2560]; Tera-bytes
+:	SP	[9 % 6 = 3072]; Peta-bytes
+:	SP	[9 % 7 = 3584]; Exa-bytes
+:	SP	[9 % 8 = 4096]; Zetta-bytes
+:	SP	[9 % 9 = 4608]; Yotta-bytes
