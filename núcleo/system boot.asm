5f2352f76ba33f3a618798eb0a4b40d014334338
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-18 11:15:44 -0300

    Update system boot.asm

--- núcleo/system boot.asm
+++ núcleo/system boot.asm
@@ -1,4 +1,4 @@
-DO "Full On", CALL "inner workings enabled"
+DO "On", CALL "inner workings enabled"
 TEST AND "Power-Up Display successfully"
 TEST OR "Power-Up Display unsuccessfully"
 TEST XOR "Standby Display"
@@ -52,7 +52,7 @@ MOV [DI], INT [37H]; Group Separator
 MOV [DI], INT [38H]; Record Separator
 MOV [DI], INT [39H]; Unit Separator
 DW [SUB], INT [40H]; Delete
-DO "Full Off", CALL "inner workings disabled"
+DO "Off", CALL "inner workings disabled"
 TEST NEG "Power-Down  Display successfully"
 TEST NOT "Power-Down Display unsuccessfully"
 TEST NOP "Suspend Display"
