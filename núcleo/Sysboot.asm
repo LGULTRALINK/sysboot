fbaac09baa610d6448dc39cec8e7520d431ae118
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-18 10:35:49 -0300

    Update Sysboot.asm

--- núcleo/Sysboot.asm
+++ núcleo/Sysboot.asm
@@ -1,8 +1,8 @@
-DO CALL  "turn on device"
+DO "turn on device"
 TEST AND "Power-Up Display successfully"
 TEST OR "Power-Up Display unsuccessfully"
 TEST XOR "Standby Display"
-RET "is working"
+CALL "is working"
 MOVS [EAX], MOVSW "space access";
 MOV [AX/DX], IN/OUT [00H]; Null
 MOV [AX/DX], IN/OUT [01H]; Start of Header
@@ -25,35 +25,35 @@ MOV [BH/BL], IN/OUT [14H]; Partial Line Foreward
 MOV [BH/BL], IN/OUT [15H]; Partial Line Backward
 MOV [BH/BL], IN/OUT [16H]; Set Transmit State
 MOVS [EDX], MOVSW "data space";
-MOV [DS], IN/IN [17H]; Horizontal Tabulation
-MOV [DS], IN/IN [18H]; Line Feed
-MOV [DS], IN/IN [19H]; Vertical Tabulation
-MOV [DS], IN/IN [20H]; Form Feed
+MOV [DS], RET IN [17H]; Horizontal Tabulation
+MOV [DS], RET IN [18H]; Line Feed
+MOV [DS], RET IN [19H]; Vertical Tabulation
+MOV [DS], RET IN [20H]; Form Feed
 MOVS [ECX], MOVSW "code space";
-MOV [CS], OUT [21H]; Carriage Return
-MOV [CS], OUT [22H]; Shift Out
-MOV [CS], OUT [23H]; Shift In
-MOV [CS], OUT [24H]; Data Link Escape
+MOV [CS], RET OUT [21H]; Carriage Return
+MOV [CS], RET OUT [22H]; Shift Out
+MOV [CS], RET OUT [23H]; Shift In
+MOV [CS], RET OUT [24H]; Data Link Escape
 MOVS [EBP], MOVSW "stack base space";
-MOV [ES], IN/OUT [25H]; Device Control 1
-MOV [ES], IN/OUT [26H]; Device Control 2
-MOV [ES], IN/OUT [27H]; Device Control 3
-MOV [ES], IN/OUT [28H]; Device Control 4
+MOV [ES], RET OUT [25H]; Device Control 1
+MOV [ES], RET IN [26H]; Device Control 2
+MOV [ES], RET OUT [27H]; Device Control 3
+MOV [ES], RET IN [28H]; Device Control 4
 MOVS [ESP], MOVSW "stack source space";
-MOV [SS], IN/OUT [29H]; Negative Acknowledge
-MOV [SS], IN/OUT [30H]; Synchronous Idle
-MOV [SS], IN/OUT [31H]; End of Transmission Block
-MOV [SS], IN/OUT [32H]; Cancel
+MOV [SS], IRET [29H]; Negative Acknowledge
+MOV [SS], IRET [30H]; Synchronous Idle
+MOV [SS], IRET [31H]; End of Transmission Block
+MOV [SS], IRET [32H]; Cancel
 MOVS [EIP], MOVSW "program index space"
-MOV [SI], IN/OUT [33H]; End of Medium
-MOV [SI], IN/OUT [34H]; Substitute
-MOV [SI], IN/OUT [35H]; Escape
-MOV [DI], IN/OUT [36H]; File Separator
-MOV [DI], IN/OUT [37H]; Group Separator
-MOV [DI], IN/OUT [38H]; Record Separator
-MOV [DI], IN/OUT [39H]; Unit Separator
-DO CALL "turn off device"
+MOV [SI], IRET [33H]; End of Medium
+MOV [SI], IRET [34H]; Substitute
+MOV [SI], IRET [35H]; Escape
+MOV [DI], IRET [36H]; File Separator
+MOV [DI], IRET [37H]; Group Separator
+MOV [DI], IRET [38H]; Record Separator
+MOV [DI], IRET [39H]; Unit Separator
+DO "turn off device"
 TEST NEG "Power-Down  Display successfully"
 TEST NOT "Power-Down Display unsuccessfully"
 TEST NOP "Suspend Display"
-RET "it is not working"
+CALL "it is not working"
