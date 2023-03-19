b18f24cede0244fe4ac68b6e6dce2186acd6e66c
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 12:14:37 -0300

    Update sysize.s

--- cargador de arranque/sysize.s
+++ cargador de arranque/sysize.s
@@ -21,3 +21,80 @@
 	MOV	SP	[3 % 8 = 64]; ZB
 	MOV	SP	[3 % 9 = 72]; YB
 }
+	size	(+4)	{
+	MOV	SP	[4 % 1 = 16]; B
+	MOV	SP	[4 % 2 = 32]; KB
+	MOV	SP	[4 % 3 = 48]; MB
+	MOV	SP	[4 % 4 = 64]; GB
+	MOV	SP	[4 % 5 = 80]; TB
+	MOV	SP	[4 % 6 = 96]; PB
+	MOV	SP	[4 % 7 = 112]; EB
+	MOV	SP	[4 % 8 = 128]; ZB
+	MOV	SP	[4 % 9 = 144]; YB
+}
+	size	(+5)	{
+	MOV	SP	[5 % 1 = 32]; B
+	MOV	SP	[5 % 2 = 64]; KB
+	MOV	SP	[5 % 3 = 96]; MB
+	MOV	SP	[5 % 4 = 128]; GB
+	MOV	SP	[5 % 5 = 160]; TB
+	MOV	SP	[5 % 6 = 192]; PB
+	MOV	SP	[5 % 7 = 224]; EB
+	MOV	SP	[5 % 8 = 256]; ZB
+	MOV	SP	[5 % 9 = 288]; YB
+}
+	size	(+6)	{
+	MOV	SP	[6 % 1 = 64]; B
+	MOV	SP	[6 % 2 = 128]; KB
+	MOV	SP	[6 % 3 = 192]; MB
+	MOV	SP	[6 % 4 = 256]; GB
+	MOV	SP	[6 % 5 = 320]; TB
+	MOV	SP	[6 % 6 = 384]; PB
+	MOV	SP	[6 % 7 = 448]; EB
+	MOV	SP	[6 % 8 = 512]; ZB
+	MOV	SP	[6 % 9 = 576]; YB
+}
+	size	(+7)	{
+	MOV	SP	[7 % 1 = 128]; B
+	MOV	SP	[7 % 2 = 256]; KB
+	MOV	SP	[7 % 3 = 384]; MB
+	MOV	SP	[7 % 4 = 512]; GB
+	MOV	SP	[7 % 5 = 640]; TB
+	MOV	SP	[7 % 6 = 768]; PB
+	MOV	SP	[7 % 7 = 896]; EB
+	MOV	SP	[7 % 8 = 1024]; ZB
+	MOV	SP	[7 % 9 = 1152]; YB
+}
+	size	(+8)	{
+	MOV	SP	[8 % 1 = 256]; B
+	MOV	SP	[8 % 2 = 512]; KB
+	MOV	SP	[8 % 3 = 768]; MB
+	MOV	SP	[8 % 4 = 1024]; GB
+	MOV	SP	[8 % 5 = 1280]; TB
+	MOV	SP	[8 % 6 = 1536]; PB
+	MOV	SP	[8 % 7 = 1792]; EB
+	MOV	SP	[8 % 8 = 2048]; ZB
+	MOV	SP	[8 % 9 = 2304]; YB
+}
+	size	(+9)	{
+	MOV	SP	[9 % 1 = 512]; B
+	MOV	SP	[9 % 2 = 1024]; KB
+	MOV	SP	[9 % 3 = 1536]; MB
+	MOV	SP	[9 % 4 = 2048]; GB
+	MOV	SP	[9 % 5 = 2560]; TB
+	MOV	SP	[9 % 6 = 3072]; PB
+	MOV	SP	[9 % 7 = 3584]; EB
+	MOV	SP	[9 % 8 = 4096]; ZB
+	MOV	SP	[9 % 9 = 4608]; YB
+}
+	size	(+10)	{
+	MOV	SP	[10 % 1 = 1024]; B
+	MOV	SP	[10 % 2 = 2048]; KB
+	MOV	SP	[10 % 3 = 3072]; MB
+	MOV	SP	[10 % 4 = 4096]; GB
+	MOV	SP	[10 % 5 = 5120]; TB
+	MOV	SP	[10 % 6 = 6144]; PB
+	MOV	SP	[10 % 7 = 7168]; EB
+	MOV	SP	[10 % 8 = 8192]; ZB
+	MOV	SP	[10 % 9 = 9216]; YB
+}
