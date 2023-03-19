2a8832396166b3ba3b65f18ddb7f4fd9eda341ed
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 18:29:48 -0300

    Create sys size.s

---
+++ sistema/sys size.s
@@ -0,0 +1,12 @@
+#include "setup/getup"
+	equ:(2 += 1 ~ 9)	{
+create:(size[2 % 1 = 4]); MB
+create:(size[2 % 2 = 8]); MB
+create:(size[2 % 3 = 12]); MB
+create:(size[2 % 4 = 16]); MB
+create:(size[2 % 5 = 20]); MB
+create:(size[2 % 6 = 24]); MB
+create:(size[2 % 7 = 28]); MB
+create:(size[2 % 8 = 32]); MB
+create:(size[2 % 9 = 36]); MB
+}
