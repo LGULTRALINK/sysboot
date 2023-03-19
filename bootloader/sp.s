3eef288d3542f23c29dbc55026038e5eca096521
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 11:11:04 -0300

    Update sp.s

--- bootloader/sp.s
+++ bootloader/sp.s
@@ -1,111 +1,111 @@
-#include "machine language"
 #include "assembly language"
-:	SP	[2 << % 1 = 4];
-:	SP	[2 << % 2 = 8];
-:	SP	[2 << % 3 = 12];
-:	SP	[2 << % 4 = 16];
-:	SP	[2 << % 5 = 20];
-:	SP	[2 << % 6 = 24];
-:	SP	[2 << % 7 = 28];
-:	SP	[2 << % 8 = 32];
-:	SP	[2 << % 9 = 36]; 
-* this line isblank
-:	SP	[3 % 1 = 8];  B
-:	SP	[3 % 2 = 16]; KB
-:	SP	[3 % 3 = 24]; MB
-:	SP	[3 % 4 = 32]; GB
-:	SP	[3 % 5 = 40]; TB
-:	SP	[3 % 6 = 48]; PB
-:	SP	[3 % 7 = 56]; EB
-:	SP	[3 % 8 = 64]; ZB
-:	SP	[3 % 9 = 72]; YB
-* this line isblank
-:	SP	[4 % 1 = 16];  B
-:	SP	[4 % 2 = 32]; KB
-:	SP	[4 % 3 = 48]; MB
-:	SP	[4 % 4 = 64]; GB
-:	SP	[4 % 5 = 80]; TB
-:	SP	[4 % 6 = 96]; PB
-:	SP	[4 % 7 = 112]; EB
-:	SP	[4 % 8 = 128]; ZB
-:	SP	[4 % 9 = 144]; YB
-* this line isblank
-:	SP	[5 % 1 = 32];  B
-:	SP	[5 % 2 = 64]; KB
-:	SP	[5 % 3 = 96]; MB
-:	SP	[5 % 4 = 128]; GB
-:	SP	[5 % 5 = 160]; TB
-:	SP	[5 % 6 = 192]; PB
-:	SP	[5 % 7 = 224]; EB
-:	SP	[5 % 8 = 256]; ZB
-:	SP	[5 % 9 = 288]; YB
-* this line isblank
-:	SP	[6 % 1 = 64];  B
-:	SP	[6 % 2 = 128]; KB
-:	SP	[6 % 3 = 192]; MB
-:	SP	[6 % 4 = 256]; GB
-:	SP	[6 % 5 = 320]; TB
-:	SP	[6 % 6 = 384]; PB
-:	SP	[6 % 7 = 448]; EB
-:	SP	[6 % 8 = 512]; ZB
-:	SP	[6 % 9 = 576]; YB
-* this line isblank
-:	SP	[7 % 1 = 128];  B
-:	SP	[7 % 2 = 256]; KB
-:	SP	[7 % 3 = 384]; MB
-:	SP	[7 % 4 = 512]; GB
-:	SP	[7 % 5 = 640]; TB
-:	SP	[7 % 6 = 768]; PB
-:	SP	[7 % 7 = 896]; EB
-:	SP	[7 % 8 = 1024]; ZB
-:	SP	[7 % 9 = 1152]; YB
-* this line isblank
-:	SP	[8 % 1 = 256];  B
-:	SP	[8 % 2 = 512]; KB
-:	SP	[8 % 3 = 768]; MB
-:	SP	[8 % 4 = 1024]; GB
-:	SP	[8 % 5 = 1280]; TB
-:	SP	[8 % 6 = 1536]; PB
-:	SP	[8 % 7 = 1792]; EB
-:	SP	[8 % 8 = 2048]; ZB
-:	SP	[8 % 9 = 2304]; YB
-* this line isblank
-:	SP	[9 % 1 = 512];  B
-:	SP	[9 % 2 = 1024]; KB
-:	SP	[9 % 3 = 1536]; MB
-:	SP	[9 % 4 = 2048]; GB
-:	SP	[9 % 5 = 2560]; TB
-:	SP	[9 % 6 = 3072]; PB
-:	SP	[9 % 7 = 3584]; EB
-:	SP	[9 % 8 = 4096]; ZB
-:	SP	[9 % 9 = 4608]; YB
-* this line isblank
-:	SP	[10 % 1 = 1024]; B
-:	SP	[10 % 2 = 2048]; KB
-:	SP	[10 % 3 = 3072]; MB
-:	SP	[10 % 4 = 4096]; GB
-:	SP	[10 % 5 = 5120]; TB
-:	SP	[10 % 6 = 6144]; PB
-:	SP	[10 % 7 = 7168]; EB
-:	SP	[10 % 8 = 8192]; ZB
-:	SP	[10 % 9 = 9216]; YB
-* this line isblank
-:	SP	[11 % 1 = 2048]; B
-:	SP	[11 % 2 = 4096]; KB
-:	SP	[11 % 3 = 6144]; MB
-:	SP	[11 % 4 = 8192]; GB
-:	SP	[11 % 5 = 10240]; TB
-:	SP	[11 % 6 = 12288]; PB
-:	SP	[11 % 7 = 14336]; EB
-:	SP	[11 % 8 = 16384]; ZB
-:	SP	[11 % 9 = 18432]; YB
-* this line isblank
-:	SP	[12 % 1 = 4096]; B
-:	SP	[12 % 2 = 8192]; KB
-:	SP	[12 % 3 = 12288]; MB
-:	SP	[12 % 4 = 16384]; GB
-:	SP	[12 % 5 = 20480]; TB
-:	SP	[12 % 6 = 24576]; PB
-:	SP	[12 % 7 = 28672]; EB
-:	SP	[12 % 8 = 32768]; ZB
-:	SP	[12 % 9 = 36864]; YB
+/ * this line is blank * /
+:	MOV SP	[2 << % 1 = 4];
+:	MOV SP	[2 << % 2 = 8];
+:	MOV SP	[2 << % 3 = 12];
+:	MOV SP	[2 << % 4 = 16];
+:	MOV SP	[2 << % 5 = 20];
+:	MOV SP	[2 << % 6 = 24];
+:	MOV SP	[2 << % 7 = 28];
+:	MOV SP	[2 << % 8 = 32];
+:	MOV SP	[2 << % 9 = 36]; 
+/ * this line is blank * /
+:	MOV SP	[3 << % 1 = 8];
+:	MOV SP	[3 << % 2 = 16];
+:	MOV SP	[3 << % 3 = 24];
+:	MOV SP	[3 << % 4 = 32];
+:	MOV SP	[3 << % 5 = 40];
+:	MOV SP	[3 << % 6 = 48];
+:	MOV SP	[3 << % 7 = 56];
+:	MOV SP	[3 << % 8 = 64];
+:	MOV SP	[3 << % 9 = 72];
+/ * this line is blank * /
+:	MOV SP	[4 << % 1 = 16];
+:	MOV SP	[4 << % 2 = 32];
+:	MOV SP	[4 << % 3 = 48];
+:	MOV SP	[4 << % 4 = 64];
+:	MOV SP	[4 << % 5 = 80];
+:	MOV SP	[4 << % 6 = 96];
+:	MOV SP	[4 << % 7 = 112];
+:	MOV SP	[4 << % 8 = 128];
+:	MOV SP	[4 << % 9 = 144];
+/ * this line is blank * /
+:	MOV SP	[5 << % 1 = 32];
+:	MOV SP	[5 << % 2 = 64];
+:	MOV SP	[5 << % 3 = 96];
+:	MOV SP	[5 << % 4 = 128];
+:	MOV SP	[5 << % 5 = 160];
+:	MOV SP	[5 << % 6 = 192];
+:	MOV SP	[5 << % 7 = 224];
+:	MOV SP	[5 << % 8 = 256];
+:	MOV SP	[5 << % 9 = 288];
+/ * this line is blank * /
+:	MOV SP	[6 << % 1 = 64];
+:	MOV SP	[6 << % 2 = 128];
+:	MOV SP	[6 << % 3 = 192];
+:	MOV SP	[6 << % 4 = 256];
+:	MOV SP	[6 << % 5 = 320];
+:	MOV SP	[6 << % 6 = 384];
+:	MOV SP	[6 << % 7 = 448];
+:	MOV SP	[6 << % 8 = 512];
+:	MOV SP	[6 << % 9 = 576];
+/ * this line is blank * /
+:	MOV SP	[7 << % 1 = 128];
+:	MOV SP	[7 << % 2 = 256];
+:	MOV SP	[7 << % 3 = 384];
+:	MOV SP	[7 << % 4 = 512];
+:	MOV SP	[7 << % 5 = 640];
+:	MOV SP	[7 << % 6 = 768];
+:	MOV SP	[7 << % 7 = 896];
+:	MOV SP	[7 << % 8 = 1024];
+:	MOV SP	[7 << % 9 = 1152];
+/ * this line is blank * /
+:	MOV SP	[8 << % 1 = 256];
+:	MOV SP	[8 << % 2 = 512];
+:	MOV SP	[8 << % 3 = 768];
+:	MOV SP	[8 << % 4 = 1024];
+:	MOV SP	[8 << % 5 = 1280];
+:	MOV SP	[8 << % 6 = 1536];
+:	MOV SP	[8 << % 7 = 1792];
+:	MOV SP	[8 << % 8 = 2048];
+:	MOV SP	[8 << % 9 = 2304];
+/ * this line is blank * /
+:	MOV SP	[9 << % 1 = 512];
+:	MOV SP	[9 << % 2 = 1024];
+:	MOV SP	[9 << % 3 = 1536];
+:	MOV SP	[9 << % 4 = 2048];
+:	MOV SP	[9 << % 5 = 2560];
+:	MOV SP	[9 << % 6 = 3072];
+:	MOV SP	[9 << % 7 = 3584];
+:	MOV SP	[9 << % 8 = 4096];
+:	MOV SP	[9 << % 9 = 4608];
+/ * this line is blank * /
+:	MOV SP	[10 << % 1 = 1024];
+:	MOV SP	[10 << % 2 = 2048];
+:	MOV SP	[10 << % 3 = 3072];
+:	MOV SP	[10 << % 4 = 4096];
+:	MOV SP	[10 << % 5 = 5120];
+:	MOV SP	[10 << % 6 = 6144];
+:	MOV SP	[10 << % 7 = 7168];
+:	MOV SP	[10 << % 8 = 8192];
+:	MOV SP	[10 << % 9 = 9216];
+/ * this line is blank * /
+:	MOV SP	[11 << % 1 = 2048];
+:	MOV SP	[11 << % 2 = 4096];
+:	MOV SP	[11 << % 3 = 6144];
+:	MOV SP	[11 << % 4 = 8192];
+:	MOV SP	[11 << % 5 = 10240];
+:	MOV SP	[11 << % 6 = 12288];
+:	MOV SP	[11 << % 7 = 14336];
+:	MOV SP	[11 << % 8 = 16384];
+:	MOV SP	[11 << % 9 = 18432];
+/ * this line is blank * /
+:	MOV SP	[12 << % 1 = 4096];
+:	MOV SP	[12 << % 2 = 8192];
+:	MOV SP	[12 << % 3 = 12288];
+:	MOV SP	[12 << % 4 = 16384];
+:	MOV SP	[12 << % 5 = 20480];
+:	MOV SP	[12 << % 6 = 24576];
+:	MOV SP	[12 << % 7 = 28672];
+:	MOV SP	[12 << % 8 = 32768];
+:	MOV SP	[12 << % 9 = 36864];
