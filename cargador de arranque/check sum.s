ea425db32c289a007dd7b26f12affba175afa548
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 15:51:05 -0300

    Update and rename checksum.s to check sum.s

---
+++ cargador de arranque/check sum.s
@@ -0,0 +1,24 @@
+#include "checksum"
+checkin:(0 ~ 9)	{
+sum:	val	[0 + $ 0 = 0x00];
+sum:	val	[0 + $ 1 = 0x10];
+sum:	val	[0 + $ 2 = 0x20];
+sum:	val	[0 + $ 3 = 0x30];
+sum:	val	[0 + $ 4 = 0x40];
+sum:	val	[0 + $ 5 = 0x50];
+sum:	val	[0 + $ 6 = 0x60];
+sum:	val	[0 + $ 7 = 0x70];
+sum:	val	[0 + $ 8 = 0x80];
+sum:	val	[0 + $ 9 = 0x90];
+}
+checkin:(10 ~ 19)	{
+sum:	val	[0 + $ 10 = 0x100];
+sum:	val	[0 + $ 11 = 0x110];
+sum:	val	[0 + $ 12 = 0x120];
+sum:	val	[0 + $ 13 = 0x130];
+sum:	val	[0 + $ 14 = 0x140];
+sum:	val	[0 + $ 15 = 0x150];
+sum:	val	[0 + $ 16 = 0x160];
+sum:	val	[0 + $ 17 = 0x170];
+sum:	val	[0 + $ 18 = 0x180];
+}
