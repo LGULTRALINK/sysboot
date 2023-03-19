689ab6d68c1d5a4f27170396626284c8b1bc8d7a
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 15:28:39 -0300

    Update checksum.s

--- cargador de arranque/checksum.s
+++ cargador de arranque/checksum.s
@@ -1,13 +1,13 @@
 #include "assembly language"
 checkin:	{
-sum:	MOV	DS	[0 + $ 0 = 0x00];
-sum:	MOV	DS	[0 + $ 1 = 0x10];
-sum:	MOV	DS	[0 + $ 2 = 0x20];
-sum:	MOV	DS	[0 + $ 3 = 0x30];
-sum:	MOV	DS	[0 + $ 4 = 0x40];
-sum:	MOV	DS	[0 + $ 5 = 0x50];
-sum:	MOV	DS	[0 + $ 6 = 0x60];
-sum:	MOV	DS	[0 + $ 7 = 0x70];
-sum:	MOV	DS	[0 + $ 8 = 0x80];
-sum:	MOV	DS	[0 + $ 9 = 0x90];
+sum:	val	[0 + $ 0 = 0x00];
+sum:	val	[0 + $ 1 = 0x10];
+sum:	val	[0 + $ 2 = 0x20];
+sum:	val	[0 + $ 3 = 0x30];
+sum:	val	[0 + $ 4 = 0x40];
+sum:	val	[0 + $ 5 = 0x50];
+sum:	val	[0 + $ 6 = 0x60];
+sum:	val	[0 + $ 7 = 0x70];
+sum:	val	[0 + $ 8 = 0x80];
+sum:	val	[0 + $ 9 = 0x90];
 }
