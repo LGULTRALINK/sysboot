fc3c78934170740b9c73e6bce9ed229780377f61
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 11:34:18 -0300

    Update and rename data percent.s to data size.s

---
+++ boot loader/data size.s
@@ -0,0 +1,11 @@
+	size	of(×2)	{
+	SP	[2 % 1 = 4] /* byte */
+	SP	[2 % 2 = 8] /* kilobyte */
+	SP	[2 % 3 = 12] /* megabyte */
+	SP	[2 % 4 = 16] /* gigabyte */
+	SP	[2 % 5 = 20] /* terabyte */
+	SP	[2 % 6 = 24] /* petabyte */
+	SP	[2 % 7 = 28] /* exabyte */
+	SP	[2 % 8 = 32] /* zettabyte */
+	SP	[2 % 9 = 36] /* yottabyte */
+}
