88748a04835436cd2f8383ba904277d6e36f7a8d
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 18:33:04 -0300

    Update sys size.s

--- sistema/sys size.s
+++ sistema/sys size.s
@@ -1,12 +1,12 @@
 #include "setup/getup"
 	equ:(2 += 1 ~ 9)	{
-create:(size[2 % 1 = 0x4]); addr
-create:(size[2 % 2 = 8]); MB
-create:(size[2 % 3 = 12]); MB
-create:(size[2 % 4 = 16]); MB
-create:(size[2 % 5 = 20]); MB
-create:(size[2 % 6 = 24]); MB
-create:(size[2 % 7 = 28]); MB
-create:(size[2 % 8 = 32]); MB
-create:(size[2 % 9 = 36]); MB
+size:([2 % 1 = 4]); 
+size:([2 % 2 = 8]); 
+size:([2 % 3 = 12]); 
+size:([2 % 4 = 16]); 
+size:([2 % 5 = 20]); 
+size:([2 % 6 = 24]); 
+size:([2 % 7 = 28]); 
+size:([2 % 8 = 32]); 
+size:([2 % 9 = 36]); 
 }
