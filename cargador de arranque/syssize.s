a7392d5181ffbf693ec108423ce97e92a42fb18b
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 11:44:11 -0300

    Update and rename boot loader/data size.s to cargador de arranque/syssize.s

---
+++ cargador de arranque/syssize.s
@@ -0,0 +1,12 @@
+#include = "assembly language"
+	size	(x2)	{
+	MOV	SP	[2 % 1 = 4]; B
+	MOV	SP	[2 % 2 = 8]; KB
+	MOV	SP	[2 % 3 = 12]; MB
+	MOV	SP	[2 % 4 = 16]; GB
+	MOV	SP	[2 % 5 = 20]; TB
+	MOV	SP	[2 % 6 = 24]; PB
+	MOV	SP	[2 % 7 = 28]; EB
+	MOV	SP	[2 % 8 = 32]; ZB
+	MOV	SP	[2 % 9 = 36]; YB
+}
