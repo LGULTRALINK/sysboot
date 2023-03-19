df3c80ad9460001fd84f0a134e4f0a94f81f4f33
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 11:40:21 -0300

    Update data size.s

--- boot loader/data size.s
+++ boot loader/data size.s
@@ -1,11 +1,11 @@
-	size	of(×2)	{
-	SP	[2 % 1 = 4] /* byte */
-	SP	[2 % 2 = 8] /* kilobyte */
-	SP	[2 % 3 = 12] /* megabyte */
-	SP	[2 % 4 = 16] /* gigabyte */
-	SP	[2 % 5 = 20] /* terabyte */
-	SP	[2 % 6 = 24] /* petabyte */
-	SP	[2 % 7 = 28] /* exabyte */
-	SP	[2 % 8 = 32] /* zettabyte */
-	SP	[2 % 9 = 36] /* yottabyte */
+	size	for(addr)	{
+	SP	[2 % 1 = 4]; B
+	SP	[2 % 2 = 8]; KB
+	SP	[2 % 3 = 12]; MB
+	SP	[2 % 4 = 16]
+	SP	[2 % 5 = 20]
+	SP	[2 % 6 = 24]
+	SP	[2 % 7 = 28]
+	SP	[2 % 8 = 32]
+	SP	[2 % 9 = 36]
 }
