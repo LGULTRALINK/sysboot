6095eb11ed14be7579d1a0ebe391780ae4e5f534
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 11:53:47 -0300

    Update sysize.s

--- cargador de arranque/sysize.s
+++ cargador de arranque/sysize.s
@@ -1,5 +1,5 @@
 #include = "assembly language"
-	size	(x2)	{
+	size	(+2)	{
 	MOV	SP	[2 % 1 = 4]; B
 	MOV	SP	[2 % 2 = 8]; KB
 	MOV	SP	[2 % 3 = 12]; MB
@@ -9,4 +9,15 @@
 	MOV	SP	[2 % 7 = 28]; EB
 	MOV	SP	[2 % 8 = 32]; ZB
 	MOV	SP	[2 % 9 = 36]; YB
+}
+	size	(+3)	{
+	MOV	SP	[3 % 1 = 8]; B
+	MOV	SP	[3 % 2 = 16]; KB
+	MOV	SP	[3 % 3 = 24]; MB
+	MOV	SP	[3 % 4 = 32]; GB
+	MOV	SP	[3 % 5 = 40]; TB
+	MOV	SP	[3 % 6 = 48]; PB
+	MOV	SP	[3 % 7 = 56]; EB
+	MOV	SP	[3 % 8 = 64]; ZB
+	MOV	SP	[3 % 9 = 72]; YB
 }
