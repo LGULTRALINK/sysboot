6963b376268176722fca541510042faf2dd41995
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-18 23:41:55 -0300

    Update sp.s

--- bootloader/sp.s
+++ bootloader/sp.s
@@ -1,6 +1,6 @@
 #include "machine language"
-#include "filename // sp.s"
-#include "readable file"
+#include "assembly language"
+#include "compiler language"
 :	SP	[2 % 1 = 4]; B
 :	SP	[2 % 2 = 8]; KB
 :	SP	[2 % 3 = 12]; MB
