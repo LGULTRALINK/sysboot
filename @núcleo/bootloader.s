1a9e6bfb90fd7f5f46b79a762d77ab3829897006
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 20:22:52 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -14,56 +14,58 @@ add [], add [].
 /* this line is blank */
 load: "royal mode", call "true";
 /* move to access high and low */
-in [%ax], mov [%ah], mov [%al]
+call [%ax], mov [%ah], mov [%al]
 /* move to base high and low */
-in [%bx], mov [%bh], mov [%bl]
+call [%bx], mov [%bh], mov [%bl]
 /* move to code high and low */
-in [%cx], mov [%ch], mov [%cl]
+call [%cx], mov [%ch], mov [%cl]
 /* move to high and low data */
-in [%dx], mov [%dh], mov [%dl]
+call [%dx], mov [%dh], mov [%dl]
 /* move stack item */
-in [cmp], mov [%ss], mov [%es]
+call [cmp], mov [%ss], mov [%es]
 /* this line isblank */
 load: "protected mode";
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00000111]; "\a" /* Bell */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001000]; "\b" /* BackSpace */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001001]; "\t" /* Horizontal Tabulation */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001010]; "\n" /* Line Feed  */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001011]; "\v" /* Vertical Tabulation */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001100]; "\f" /* Form Feed */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
-out [%ebp], pop [%ecx]
-out [%esp], pop [%edx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
+out [%ebp], in [%ecx]
+out [%esp], in [%edx]
 out [%eip], byte: [00001101]; "\r" /* Carriage Return */
-out [%esi], pop [%eax]
-out [%edi], pop [%ebx]
+out [%esi], in [%eax]
+out [%edi], in [%ebx]
 /* Horizontal writing cursor */
 movsw "\p", movsb "%d"
 /* vertical reading cursor */
