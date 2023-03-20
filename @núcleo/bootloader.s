97fe76ddfd532afbfc6cf6b61bdaafb2c3505904
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 14:37:14 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,12 +1,12 @@
 #include "assembly language"
-make: runtime = ("On") > [+1,+2,+3,+4,+5,+6,+7,+8,9,]
-break: runtime = ("On") < [-1,-2,-3,-4,-5,-6,-7,-8,9,]
-add %esp[],mov size =();
-add %ebp[]
-add %esi[]
-add %edi[]
-add %eax[]
-add %ebx[]
-add %ecx[]
-add %edx[]
-add %eip[]
+make: runtime = ("On") >> [+1,+2,+3,+4,+5,+6,+7,+8,9,]
+break: runtime = ("On") << [-1,-2,-3,-4,-5,-6,-7,-8,9,]
+add: %esp[],mov size = ;
+add: %ebp[],mov size = ;
+add: %esi[],mov size = ;
+add: %edi[],mov size = ;
+add: %eax[],mov size = ;
+add: %ebx[],mov size = ;
+add: %ecx[],mov size = ;
+add: %edx[],mov size = ;
+add: %eip[],mov size = ;
