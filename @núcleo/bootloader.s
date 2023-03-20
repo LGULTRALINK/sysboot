e721cd194e743ab7b4cdccff10f384a954ccead2
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 15:02:03 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,9 +1,9 @@
 ##include = "assembly language"
-make: runtime = ("On") else: ("Off");
-break: runtime = ("On") else: ("Off");
+make: runtime = ("On") switch: ("Off");
+break: runtime = ("On") switch: ("Off");
 add: %ebp[0 +$ 1 = 0x10], push [2 < 1 = 0x4];
 add: %esp[0 +$ 2 = 0x20], push [2 < 2 = 0x8];
-add: %esi[0 +$ 3 = 0x30], push [];
+add: %esi[0 +$ 3 = 0x30], push [2 < 3 = 0x12];
 add: %edi[0 +$ 4 = 0x40], push [];
 add: %eax[0 +$ 5 = 0x50], push [];
 add: %ebx[0 +$ 6 = 0x60], push [];
