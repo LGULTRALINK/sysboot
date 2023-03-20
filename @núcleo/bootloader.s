e8a46eb7fc8ce344e28224cd0adc7720ad72ec85
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 15:10:22 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,14 +1,14 @@
 #include = "assembly language"
-do: "Power-Up Display" is = "Standby Display"
+do: "Power-Up Display"
 switch: ("Off/On");
-do: "Power-Down Display" is = "Suspend Display"
+do: "Power-Down Display"
 switch: ("On/Off");
-add: %ebp[0 +$ 1 = 0x10], push [2 < 1 = 0x4];
-add: %esp[0 +$ 2 = 0x20], push [2 < 2 = 0x8];
-add: %esi[0 +$ 3 = 0x30], push [2 < 3 = 0x12];
-add: %edi[0 +$ 4 = 0x40], push [];
-add: %eax[0 +$ 5 = 0x50], push [];
-add: %ebx[0 +$ 6 = 0x60], push [];
-add: %ecx[0 +$ 7 = 0x70], push [];
-add: %edx[0 +$ 8 = 0x80], push [];
-add: %eip[0 +$ 9 = 0x90], push [] ;
+add: %ebp[0 +$ 1 = 0x10], push [2 > 1 = 0x4];
+add: %esp[0 +$ 2 = 0x20], push [2 > 2 = 0x8];
+add: %esi[0 +$ 3 = 0x30], push [2 > 3 = 0x12];
+add: %edi[0 +$ 4 = 0x40], push [2 > 4 = 0x16];
+add: %eax[0 +$ 5 = 0x50], push [2 > 5 = 0x20];
+add: %ebx[0 +$ 6 = 0x60], push [2 > 6 = 0x24];
+add: %ecx[0 +$ 7 = 0x70], push [2 > 7 = 0x28];
+add: %edx[0 +$ 8 = 0x80], push [2 > 8 = 0x32];
+add: %eip[0 +$ 9 = 0x90], push [2 > 9 = 0x36];
