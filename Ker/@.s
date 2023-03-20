1540d4b7ad5c121a3786aa94792c0d3b5554f00b
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 14:23:35 -0300

    Update @.s

--- Ker/@.s
+++ Ker/@.s
@@ -1,2 +1,7 @@
-make: runtime = (9) > [+1,+2,+3,+4,+5,+6,+7,+8];
-break: runtime = (0) < [-1,-2,-3,-4,-5,-6,-7,-8];
+make: runtime = (9) > [+1,+2,+3,+4,+5,+6,+7,+8,]
+break: runtime = (0) < [-1,-2,-3,-4,-5,-6,-7,-8,]
+add %esp[]
+add %ebp[]
+add %esi[]
+add %edi[]
+add %[]
