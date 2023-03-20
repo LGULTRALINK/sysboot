cb4b249c313ad02e1b20a25f5f627a6a11988b35
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 15:04:55 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,7 +1,7 @@
-##include = "assembly language"
-/* Power-Up Display * /
+#include = "assembly language"
+iret "Power-Up Display"
 switch: ("Off/On");
-/* Power-Down Display */
+iret "Power-Down Display"
 switch: ("On/Off");
 add: %ebp[0 +$ 1 = 0x10], push [2 < 1 = 0x4];
 add: %esp[0 +$ 2 = 0x20], push [2 < 2 = 0x8];
