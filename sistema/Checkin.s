c5e62c4366beef5b87c1b7122881688fb1617e85
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 12:45:45 -0300

    Update and rename Binary Digits.s to Checkin.s

---
+++ sistema/Checkin.s
@@ -0,0 +1,12 @@
+checkin(equ = 2 of 1 ~ 9) : {
+add_size:	[2 < 0 = 0]; NUL
+add_size:	[2 < 1 = 4]; B
+add_size:	[2 < 2 = 8]; KB
+add_size:	[2 < 3 = 12]; MB
+add_size:	[2 < 4 = 16]; GB
+add_size:	[2 < 5 = 20]; TB
+add_size:	[2 < 6 = 24]; PB
+add_size:	[2 < 7 = 28]; EB
+add_size:	[2 < 8 = 32]; ZB
+add_size:	[2 < 9 = 36]; YB
+};
