9afedcae55f3e53b14110e807cc1030fa82f2b57
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 13:24:58 -0300

    Update space.s

--- sistema/space.s
+++ sistema/space.s
@@ -1,15 +1,15 @@
 #include "assembly language"
 Read = "interpret data"
 Write = "print data"
-checkin:(2 =  >> 0 ~ 9) {
-size:	[2 % 0 = 0x0];  // NULL
-size:	[2 % 1 = 0x4];  // Byte
-size:	[2 % 2 = 0x8];  // KiloByte
-size:	[2 % 3 = 0x12]; // MegaByte
-size:	[2 % 4 = 0x16]; // GigaByte
-size:	[2 % 5 = 0x20]; // TeraByte
-size:	[2 % 6 = 0x24]; // PetaByte
-size:	[2 % 7 = 0x28]; // ExaByte
-size:	[2 % 8 = 0x32]; // ZettaByte
-size:	[2 % 9 = 0x36]; // YottaByte
+checkin:("%n") {
+size:	[2 > 0 = 0x0];  // NULL
+size:	[2 > 1 = 0x4];  // Byte
+size:	[2 > 2 = 0x8];  // KiloByte
+size:	[2 > 3 = 0x12]; // MegaByte
+size:	[2 > 4 = 0x16]; // GigaByte
+size:	[2 > 5 = 0x20]; // TeraByte
+size:	[2 > 6 = 0x24]; // PetaByte
+size:	[2 > 7 = 0x28]; // ExaByte
+size:	[2 > 8 = 0x32]; // ZettaByte
+size:	[2 > 9 = 0x36]; // YottaByte
 }
