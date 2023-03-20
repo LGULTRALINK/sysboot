29126ece6fb4b8a031a6eaf2af557de1b492eef5
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 16:21:21 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -3,18 +3,28 @@ switch: ("Off/On"), for: "Power-Up Device"
 is: "Standby Display"
 switch: ("On/Off"), for: "Power-Down Device"
 is: "Suspend Display"
-add: %ebp[0 + 1 = 0x10], push [2 >> 1 = 0x4];
-add: %esp[0 + 2 = 0x20], push [2 >> 2 = 0x8];
-add: %esi[0 + 3 = 0x30], push [2 >> 3 = 0x12];
-add: %edi[0 + 4 = 0x40], push [2 >> 4 = 0x16];
-add: %eax[0 + 5 = 0x50], push [2 >> 5 = 0x20];
-add: %ebx[0 + 6 = 0x60], push [2 >> 6 = 0x24];
-add: %ecx[0 + 7 = 0x70], push [2 >> 7 = 0x28];
-add: %edx[0 + 8 = 0x80], push [2 >> 8 = 0x32];
-add: %eip[0 + 9 = 0x90], push [2 >> 9 = 0x36];
-load: "royal mode"
-load: "royal mode"
-/* move to access up and down */
+add [0 + 1 = 0x10], push [2 >> 1 = 0x4];
+add [0 + 2 = 0x20], push [2 >> 2 = 0x8];
+add [0 + 3 = 0x30], push [2 >> 3 = 0x12];
+add [0 + 4 = 0x40], push [2 >> 4 = 0x16];
+add [0 + 5 = 0x50], push [2 >> 5 = 0x20];
+add [0 + 6 = 0x60], push [2 >> 6 = 0x24];
+add [0 + 7 = 0x70], push [2 >> 7 = 0x28];
+add [0 + 8 = 0x80], push [2 >> 8 = 0x32];
+add [0 + 9 = 0x90], push [2 >> 9 = 0x36];
+load: "royal mode";
+/* move to access high and low */
 in [%ax], mov [%ah], mov [%al]
 /* move to base high and low */
-in [%bx], mov [%bh], mov [%bh]
+in [%bx], mov [%bh], mov [%bl]
+/* move to code high and low */
+in [%cx], mov [%ch], mov [%cl]
+/* move to high and low data */
+in [%dx], mov [%dh], mov [%dl]
+/* this line isblank */
+load: "protected mode";
+out [%ebp]
+out [%esp]
+out [%eip]
+out [%esi]
+out [%edi]
