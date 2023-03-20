ad5957d4db7216ade49c1815c7d771bf04467d97
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 13:10:55 -0300

    Update and rename Checkin.s to space.s

---
+++ sistema/space.s
@@ -0,0 +1,13 @@
+#include = "assembly language"
+checkin:(equ = 2 << 1 ~ 9) {
+size:	[2 % 0 = +0]; void
+size:	[2 % 1 = +4]; B
+size:	[2 % 2 = +8]; KB
+size:	[2 % 3 = +12]; MB
+size:	[2 % 4 = +16]; GB
+size:	[2 % 5 = +20]; TB
+size:	[2 % 6 = +24]; PB
+size:	[2 % 7 = +28]; EB
+size:	[2 % 8 = +32]; ZB
+size:	[2 % 9 = +36]; YB
+}
