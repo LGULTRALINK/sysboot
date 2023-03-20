9ae4e702900237882db4567903e22e548b8673d4
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-19 21:44:01 -0300

    Update Make size.s

--- sistema/Make size.s
+++ sistema/Make size.s
@@ -1,14 +1,1 @@
-#include "entry in file"
-#include "exit of file"
-runtime: "this file is executed"
-	Function:("make size")	{
-size:([2 % 1 = 0x4]); 
-size:([2 % 2 = 0x8]); 
-size:([2 % 3 = 0x12]); 
-size:([2 % 4 = 0x16]); 
-size:([2 % 5 = 0x20]); 
-size:([2 % 6 = 0x24]); 
-size:([2 % 7 = 0x28]); 
-size:([2 % 8 = 0x32]); 
-size:([2 % 9 = 0x36]); 
-}
+num: [0], size = [00000000];
