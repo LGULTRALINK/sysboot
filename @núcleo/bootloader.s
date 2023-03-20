ee7080cdaace2cd1043c26ad3642eebe8dbd14c3
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 16:32:41 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,15 +1,15 @@
 #include "assembly language"
 switch: ("Off/On"), for: "Power-Up Device"
 is: "Standby Display"
-add [0 + 1 = 0x10], push [2 >> 1 = 0x4];
-add [0 + 2 = 0x20], push [2 >> 2 = 0x8];
-add [0 + 3 = 0x30], push [2 >> 3 = 0x12];
-add [0 + 4 = 0x40], push [2 >> 4 = 0x16];
-add [0 + 5 = 0x50], push [2 >> 5 = 0x20];
-add [0 + 6 = 0x60], push [2 >> 6 = 0x24];
-add [0 + 7 = 0x70], push [2 >> 7 = 0x28];
-add [0 + 8 = 0x80], push [2 >> 8 = 0x32];
-add [0 + 9 = 0x90], push [2 >> 9 = 0x36];
+add [0 + 1 = 0x10], push [2 >> 1 = 0x4].
+add [0 + 2 = 0x20], push [2 >> 2 = 0x8].
+add [0 + 3 = 0x30], push [2 >> 3 = 0x12].
+add [0 + 4 = 0x40], push [2 >> 4 = 0x16].
+add [0 + 5 = 0x50], push [2 >> 5 = 0x20].
+add [0 + 6 = 0x60], push [2 >> 6 = 0x24].
+add [0 + 7 = 0x70], push [2 >> 7 = 0x28].
+add [0 + 8 = 0x80], push [2 >> 8 = 0x32].
+add [0 + 9 = 0x90], push [2 >> 9 = 0x36].
 load: "royal mode";
 /* move to access high and low */
 in [%ax], mov [%ah], mov [%al]
