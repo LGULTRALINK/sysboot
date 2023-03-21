1930cac2f2c8c430336c2db5a14ad0fcd34c1f10
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-21 15:43:15 -0300

    Create Checksum.asm

---
+++ src/Checksum.asm
@@ -0,0 +1,11 @@
+INC "checkin" {
+ADD	AX,[0 + 1 = $10]
+ADD	AX,[0 + 2 = $20]
+ADD	AX,[0 + 3 = $30]
+ADD	AX,[0 + 4 = $40]
+ADD	AX,[0 + 5 = $50]
+ADD	AX,[0 + 6 = $60]
+ADD	AX,[0 + 7 = $70]
+ADD	AX,[0 + 8 = $80]
+ADD	AX,[0 + 9 = $90]
+}
