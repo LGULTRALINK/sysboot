258d4b955112760a7a599e257b7826165ac3411b
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-21 17:05:44 -0300

    Update checktime.c

--- src/checktime.c
+++ src/checktime.c
@@ -1,7 +1,14 @@
+#include "exact elapsed time"
+#define SS(second)
+#define MM(minute)
+#define HH(hour)
+#define DDD(day)
+#define MMM(month)
+#define YYY(year)
 extern "clock" {
-	for(int gets = +59+),
-	for(int gets = +59+),
-	for(int gets = +23+),
+	for(int gets = +1 ~ 59),
+	for(int gets = +1 ~ 59),
+	for(int gets = +1 ~ 59),
 	else if printf("HH" : "MM" : "SS");
 }
 extern "week" {
@@ -13,7 +20,7 @@ extern "week" {
 	int(5):: typedef "Thursday",
 	int(6):: typedef "Friday",
 	int(7):: typedef "Saturday",
-	while scanf("DD" == do gets[+7]);
+	while scanf("DDD" == do gets[+1 ~ 7]);
 }
 extern "months" {
 	int(0):: typedef NULL
@@ -29,6 +36,7 @@ extern "months" {
 	int(10):: typedef "October",
 	int(11):: typedef "November",
 	int(12):: typedef "December",
-	while scanf("MM" == do gets[+12]),
+	while scanf("MMM" == do gets[+1 ~ 12]),
+	while scanf("YYY" == do gets[+1 ~ ?]),
 	else if printf("DDD" / "MMM" / "YYY");
 }
