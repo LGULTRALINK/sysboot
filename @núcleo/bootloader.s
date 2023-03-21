8ae9fce78d876a282cdad3d5e380c8611a24d4c5
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-20 23:06:30 -0300

    Update bootloader.s

--- @núcleo/bootloader.s
+++ @núcleo/bootloader.s
@@ -1,47 +1,115 @@
 #include "assembly language"
 switch: ("Off/On"), for: "Power On Device"
 is: "Standby Display"
+size: [2 * 1 = 4]; B
+size: [2 * 2 = 8]; KB
+size: [2 * 3 = 12]; MB
+size: [2 * 4 = 16]; GB
+size: [2 * 5 = 20]; TB
+size: [2 * 6 = 24]; PB
+size: [2 * 7 = 28]; EB
+size: [2 * 8 = 32]; ZB
+size: [2 * 9 = 36]; YB
 /* this line is blank */
-add [0 + 1 = 10], mul [2 × 1 = 2].
-add [0 + 2 = 20], mul [2 × 2 = 4].
-add [0 + 3 = 30], mul [2 × 3 = 6].
-add [0 + 4 = 40], mul [2 × 4 = 8].
-add [0 + 5 = 50], mul [2 × 5 = 10].
-add [0 + 6 = 60], mul [2 × 6 = 12].
-add [0 + 7 = 70], mul [2 × 7 = 14].
-add [0 + 8 = 80], mul [2 × 8 = 16].
-add [0 + 9 = 90], mul [2 × 9 = 18].
+size: [3 * 1 = 8]; B
+size: [3 * 2 = 16]; KB
+size: [3 * 3 = 24]; MB
+size: [3 * 4 = 32]; GB
+size: [3 * 5 = 40]; TB
+size: [3 * 6 = 48]; PB
+size: [3 * 7 = 56]; EB
+size: [3 * 8 = 64]; ZB
+size: [3 * 9 = 72]; YB
 /* this line is blank */
-pop [1 + 1 = 11], push [3 × 1 = 3].
-pop [1 + 2 = 21], push [3 × 2 = 6].
-pop [1 + 3 = 31], push [3 × 3 = 9].
-pop [1 + 4 = 41], push [3 × 4 = 12].
-pop [1 + 5 = 51], push [3 × 5 = 15].
-pop [1 + 6 = 61], push [3 × 6 = 18].
-pop [1 + 7 = 71], push [3 × 7 = 21].
-pop [1 + 8 = 81], push [3 × 8 = 24].
-pop [1 + 9 = 91], push [3 × 9 = 27].
+size: [4 * 1 = 16]; B
+size: [4 * 2 = 32]; KB
+size: [4 * 3 = 48]; MB
+size: [4 * 4 = 64]; GB
+size: [4 * 5 = 80]; TB
+size: [4 * 6 = 96]; PB
+size: [4 * 7 = 112]; EB
+size: [4 * 8 = 128]; ZB
+size: [4 * 9 = 144]; YB
 /* this line is blank */
-pop [3 + 1 = 13], push [4 × 1 = 4].
-pop [3 + 2 = 23], push [4 × 2 = 8].
-pop [3 + 3 = 33], push [4 × 3 = 12].
-pop [3 + 4 = 43], push [4 × 4 = 16].
-pop [3 + 5 = 53], push [4 × 5 = 20].
-pop [3 + 6 = 63], push [4 × 6 = 24].
-pop [3 + 7 = 73], push [4 × 7 = 28].
-pop [3 + 8 = 83], push [4 × 8 = 32].
-pop [3 + 9 = 93], push [4 × 9 = 36].
+size: [5 * 1 = 32]; B
+size: [5 * 2 = 64]; KB
+size: [5 * 3 = 96]; MB
+size: [5 * 4 = 128]; GB
+size: [5 * 5 = 160]; TB
+size: [5 * 6 = 192]; PB
+size: [5 * 7 = 224]; EB
+size: [5 * 8 = 256]; ZB
+size: [5 * 9 = 288]; YB
 /* this line is blank */
-pop [4 + 1 = 14], push [5 × 1 = 5].
-pop [4 + 2 = 24], push [5 × 2 = 10].
-pop [4 + 3 = 34], push [5 × 3 = 15].
-pop [4 + 4 = 44], push [5 × 4 = 20].
-pop [4 + 5 = 54], push [5 × 5 = 25].
-pop [4 + 6 = 64], push [5 × 6 = 30].
-pop [4 + 7 = 74], push [5 × 7 = 35].
-pop [4 + 8 = 84], push [5 × 8 = 40].
-pop [4 + 9 = 94], push [5 × 9 = 45].
+size: [6 * 1 = 64]; B
+size: [6 * 2 = 128]; KB
+size: [6 * 3 = 192]; MB
+size: [6 * 4 = 256]; GB
+size: [6 * 5 = 320]; TB
+size: [6 * 6 = 384]; PB
+size: [6 * 7 = 448]; EB
+size: [6 * 8 = 512]; ZB
+size: [6 * 9 = 576]; YB
 /* this line is blank */
+size: [7 * 1 = 128]; B
+size: [7 * 2 = 256]; KB
+size: [7 * 3 = 384]; MB
+size: [7 * 4 = 512]; GB
+size: [7 * 5 = 640]; TB
+size: [7 * 6 = 768]; PB
+size: [7 * 7 = 896]; EB
+size: [7 * 8 = 1024]; ZB
+size: [7 * 9 = 1152]; YB
+/* this line is blank */
+size: [8 * 1 = 256]; B
+size: [8 * 2 = 512]; KB
+size: [8 * 3 = 768]; MB
+size: [8 * 4 = 1024]; GB
+size: [8 * 5 = 1280]; TB
+size: [8 * 6 = 1536]; PB
+size: [8 * 7 = 1792]; EB
+size: [8 * 8 = 2048]; ZB
+size: [8 * 9 = 2304]; YB
+/* this line is blank */
+size: [9 * 1 = 512]; B
+size: [9 * 2 = 1024]; KB
+size: [9 * 3 = 1536]; MB
+size: [9 * 4 = 2048]; GB
+size: [9 * 5 = 2560]; TB
+size: [9 * 6 = 3072]; PB
+size: [9 * 7 = 3584]; EB
+size: [9 * 8 = 4096]; ZB
+size: [9 * 9 = 4608]; YB
+/* this line is blank */
+size: [10 * 1 = 1024]; B
+size: [10 * 2 = 2048]; KB
+size: [10 * 3 = 3072]; MB
+size: [10 * 4 = 4096]; GB
+size: [10 * 5 = 5120]; TB
+size: [10 * 6 = 6144]; PB
+size: [10 * 7 = 7168]; EB
+size: [10 * 8 = 8192]; ZB
+size: [10 * 9 = 9216]; YB
+/* this line is blank */
+size: [11 * 1 = 2048]; B
+size: [11 * 2 = 4096]; KB
+size: [11 * 3 = 6144]; MB
+size: [11 * 4 = 8192]; GB
+size: [11 * 5 = 10240]; TB
+size: [11 * 6 = 12288]; PB
+size: [11 * 7 = 14336]; EB
+size: [11 * 8 = 16384]; ZB
+size: [11 * 9 = 18432]; YB
+/* this line is blank */
+size: [12 * 1 = 4096]; B
+size: [12 * 2 = 8192]; KB
+size: [12 * 3 = 12288]; MB
+size: [12 * 4 = 16384]; GB
+size: [12 * 5 = 20480]; TB
+size: [12 * 6 = 24576]; PB
+size: [12 * 7 = 28672]; EB
+size: [12 * 8 = 32768]; ZB
+size: [12 * 9 = 36864]; YB
 load: "royal mode"
 /* move to access high and low */
 call [%ax], mov [%ah], mov [%al]
