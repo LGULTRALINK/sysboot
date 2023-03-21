39fff892b6e585e9334b919ebf0d99c4b73593b0
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-21 15:40:40 -0300

    Update keypad.asm

--- src/keypad.asm
+++ src/keypad.asm
@@ -57,3 +57,15 @@ INT "graphic pad" {
 }
 LOCK "lock numeric keypad"
 NOT LOCK "unlock numeric keypad"
+
+INC "values in memory" {
+ADD	AX,[0 + 1 = $10]
+ADD	AX,[0 + 2 = $20]
+ADD	AX,[0 + 3 = $30]
+ADD	AX,[0 + 4 = $40]
+ADD	AX,[0 + 5 = $50]
+ADD	AX,[0 + 6 = $60]
+ADD	AX,[0 + 7 = $70]
+ADD	AX,[0 + 8 = $80]
+ADD	AX,[0 + 9 = $90]
+}
