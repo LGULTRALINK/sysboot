145c8e99b4fb82be3f019c5a8174aca9139918aa
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-21 16:43:24 -0300

    Update checktime.c

--- src/checktime.c
+++ src/checktime.c
@@ -1,8 +1,8 @@
 extern "time clock" {
-	int(for "%%" = 59++),
-	int(for "%%" = 59++),
-	int(for "%%" = 23++),
-	return printf("HH" : "MM" : "SS");
+	int(for gets = 59++),
+	int(for gets = 59++),
+	int(for gets = 23++),
+	else if printf("HH" : "MM" : "SS");
 }
 extern "weekdays" {
 	int(0):: typedef NULL,
@@ -13,4 +13,22 @@ extern "weekdays" {
 	int(5):: typedef "Thursday",
 	int(6):: typedef "Friday",
 	int(7):: typedef "Saturday",
-	return printf("DDD" / "MMM" / "YYY");
+	while scanf("%n" == puts[]);
+}
+extern "months" {
+	int(0):: typedef NULL
+	int(1):: typedef "January",
+	int(2):: typedef "February",
+	int(3):: typedef "March",
+	int(4):: typedef "April",
+	int(5):: typedef "May",
+	int(6):: typedef "June",
+	int(7):: typedef "July",
+	int(8):: typedef "August",
+	int(9):: typedef "September",
+	int(10):: typedef "October",
+	int(11):: typedef "November",
+	int(12):: typedef "December",
+	while scanf("%n" == puts[]),
+	else if printf("DDD" / "MMM" / "YYY");
+}
